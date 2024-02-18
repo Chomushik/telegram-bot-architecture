b195e2b7e3a6a3cb0ea303bdd560c82b086b51ce
Chomushik <138621038+Chomushik@users.noreply.github.com>
2024-02-18 23:50:16 +0200

    Update config_reader.py

--- bot/config_reader.py
+++ bot/config_reader.py
@@ -2,8 +2,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    BOT_TOKEN: str
     ADMIN_ID: int
+    BOT_TOKEN: str
 
     model_config = SettingsConfigDict(env_file="path_to_env_file")
 
