270199a1529e2aadcebd10d86171b0a45ab6e7d1
Chomushik <chomushik@gmail.com>
2024-02-19 00:04:51 +0200

    Update config_reader.py

--- bot/config_reader.py
+++ bot/config_reader.py
@@ -2,10 +2,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    BOT_TOKEN: str
     ADMIN_ID: int
+    BOT_TOKEN: str
 
-    model_config = SettingsConfigDict(env_file="path_to_env_file")
+    model_config = SettingsConfigDict(env_file="../.env") # The .env file is located in the root directory
 
 
 config = Settings()
